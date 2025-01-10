5461a9c4329bd8e4f1e949e7a5654454da3e8fb6
Kyle Conel <kyjordev@gmail.com>
2025-01-10 09:37:19 -0500

    Deleted files

--- C/SDLCalls/library.h
+++
@@ -1,6 +0,0 @@
-#ifndef SDLCALLS_LIBRARY_H
-#define SDLCALLS_LIBRARY_H
-
-void hello(void);
-
-#endif //SDLCALLS_LIBRARY_H
--- game.jl
+++ game.jl
@@ -2,7 +2,6 @@
 # julia +nightly --project=/home/kyjor/Documents/Projects/Julia/juliac juliac.jl --output-exe game --trim=unsafe-warn game.jl --experimental
 module Game
 libsdl2::String = joinpath(pwd(), "lib", "libSDL2.so")  
-#libcalls::String = joinpath(pwd(), "lib", "libSDLCalls.so")  
 include("lib/LibSDL2.jl")
 const cur_dir::String = pwd()
 using .LibSDL2
@@ -20,24 +19,65 @@ Base.@ccallable function main()::Cint
     surface = IMG_Load(joinpath(cur_dir, "assets", "cat.png"))
     tex = SDL_CreateTextureFromSurface(renderer, surface)
     SDL_FreeSurface(surface)
-    while keep_going
-        count += 1
-        if count == 1000000
-            keep_going = false
-        end
-        event_ref = Ref{SDL_Event}()
-        while Bool(SDL_PollEvent(event_ref))
-            evt = event_ref[]
-            println(Core.stdout, "event type = $(evt.type)")
-            if evt.type == SDL_QUIT
-                keep_going = false
-            end
-        end
 
-        SDL_Delay(10)
+    w_ref, h_ref = Ref{Cint}(0), Ref{Cint}(0)
+    SDL_QueryTexture(tex, C_NULL, C_NULL, w_ref, h_ref)
+
+    try
+        w, h = w_ref[], h_ref[]
+        x = (1000 - w) ÷ 2
+        y = (1000 - h) ÷ 2
+        dest_ref = Ref(SDL_Rect(x, y, w, h))
+        close = false
+        speed = 300
+        while !close
+            event_ref = Ref{SDL_Event}()
+            while Bool(SDL_PollEvent(event_ref))
+                evt = event_ref[]
+                evt_ty = evt.type
+                if evt_ty == SDL_QUIT
+                    close = true
+                    break
+                elseif evt_ty == SDL_KEYDOWN
+                    scan_code = evt.key.keysym.scancode
+                    if scan_code == SDL_SCANCODE_W || scan_code == SDL_SCANCODE_UP
+                        y -= speed / 30
+                        break
+                    elseif scan_code == SDL_SCANCODE_A || scan_code == SDL_SCANCODE_LEFT
+                        x -= speed / 30
+                        break
+                    elseif scan_code == SDL_SCANCODE_S || scan_code == SDL_SCANCODE_DOWN
+                        y += speed / 30
+                        break
+                    elseif scan_code == SDL_SCANCODE_D || scan_code == SDL_SCANCODE_RIGHT
+                        x += speed / 30
+                        break
+                    else
+                        break
+                    end
+                end
+            end
+    
+            x + w > 1000 && (x = 1000 - w;)
+            x < 0 && (x = 0;)
+            y + h > 1000 && (y = 1000 - h;)
+            y < 0 && (y = 0;)
+    
+            dest_ref[] = SDL_Rect(x, y, w, h)
+            SDL_RenderClear(renderer)
+            SDL_RenderCopy(renderer, tex, C_NULL, dest_ref)
+            dest = dest_ref[]
+            x, y, w, h = dest.x, dest.y, dest.w, dest.h
+            SDL_RenderPresent(renderer)
+    
+            SDL_Delay(1000 ÷ 60)
+        end
+    finally
+        SDL_DestroyTexture(tex)
+        SDL_DestroyRenderer(renderer)
+        SDL_DestroyWindow(win)
+        SDL_Quit()
     end
-    SDL_Quit()
-    println(Core.stdout, "$(count)")
     return 0
 end
 end # End of module HelloWorld 
